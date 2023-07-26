b52bcd3deeaa3698d5b7931469f3628b1b9e05dc
Nuno <nlouro@student.42heilbronn.de>
2023-07-26 11:28:01 +0200

    Add user input basic validation

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,12 +6,36 @@
 /*   By: nuno <nlouro@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/26 11:04:07 by nuno              #+#    #+#             */
-/*   Updated: 2023/07/26 11:12:51 by nuno             ###   ########.fr       */
+/*   Updated: 2023/07/26 11:27:16 by nuno             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "PmergeMe.hpp"
+
+/*
+ * accept only digits and space as input
+ */
+bool	validate_input(char *s)
+{
+	while(*s)
+	{
+		if (((int)(*s) >= '0' && (int)(*s) <= '9') || *s == ' ')
+		{
+			if (VERBOSE >= DEBUG)
+				std::cout << *s << " OK" << std::endl;
+		}
+		else
+		{
+			if (VERBOSE >= DEBUG)
+				std::cout << *s << " NOK" << std::endl;
+			std::cout << "Error" << std::endl;
+			exit(1);
+		}
+		s++;
+	}
+	return (true);
+}
 
 int main(int argc, char **argv)
 {
@@ -20,6 +44,9 @@ int main(int argc, char **argv)
 		std::cout << "Error: call like ./PmergeMe \"<positive integer sequence>\"" << std::endl;
 		return (1);
 	}
-	std::cout << argv[1] << std::endl;
+	if (validate_input(argv[1]))
+	{
+		std::cout << "Before: " << argv[1] << std::endl;
+	}
 	return (0);
 }
